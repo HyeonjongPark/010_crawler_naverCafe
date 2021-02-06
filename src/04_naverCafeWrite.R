c99bf2034155bfeb1a8196add86563b90a02cd4a
HyeonjongPark <guswhd9844@gmail.com>
2021-02-06 11:33:27 +0900

    write와 reply에 대한 전처리 함수 추가

--- src/04_naverCafeWrite.R
+++ src/04_naverCafeWrite.R
@@ -1,63 +1,146 @@
-library(lubridate)
-library(stringr)
-library(dplyr)
-library(tidyverse)
-library(padr)
+# library(lubridate)
+# library(stringr)
+# library(dplyr)
+# library(tidyverse)
+# library(padr)
 
-cafeWritten = readxl::read_excel("D:/workspace/python_workspace/01_cafeCrawler/naverCafeWrite.xlsx")
-
-# cafeWritten$title
+# cafeWritten = readxl::read_excel("D:/workspace/python_workspace/01_cafeCrawler/naverCafeWrite_A.xlsx")
 # 
-# regex
-# regmatches(cafeWritten$title[1], regexpr("[\\[].+]", cafeWritten$title[20]))
+# # cafeWritten$title
+# # 
+# # regex
+# # regmatches(cafeWritten$title[1], regexpr("[\\[].+]", cafeWritten$title[20]))
+# 
+# 
+# 
+# 
+# cafeWritten$date[str_length(cafeWritten$date) == 5] = paste0(gsub("-",".",Sys.Date()),".")
+# cafeWritten$date = ymd(cafeWritten$date)
+# 
+# start_date = "2021-01-29"
+# 
+# cafeWritten$writer = gsub("'","",cafeWritten$writer)
+# cafeWritten$writer = gsub("\\[","",cafeWritten$writer)
+# cafeWritten$writer = gsub("\\]","",cafeWritten$writer)
+# 
+# 
+# cafeWritten %>% as.data.frame()
+# tid = cafeWritten %>% group_by(writer,date) %>% summarise(n = n()) %>% 
+#   filter(date >= start_date) 
+# 
+# tid = as.data.frame(tid)
+# 
+# 
+# 
+# source("./src/06_members.R", encoding = "UTF-8")
+# 
+# 
+# for(i in 1:length(writers)) {
+#   if(tid %>% filter(writer == writers[i], date == start_date) %>% nrow() != 1) {
+#     tid = rbind(tid, data.frame(writer = writers[i], 
+#                                 date = start_date,
+#                                 n = NA))
+#     
+#   }
+#   
+#   if(tid %>% filter(writer == writers[i], date == Sys.Date()) %>% nrow() != 1) {
+#     tid = rbind(tid, data.frame(writer = writers[i], 
+#                                 date = Sys.Date(),
+#                                 n = NA))
+#     
+#   }
+#   
+#   
+# }
+# tid = tid %>% arrange(writer, date)
+# tid[is.na(tid)] = 0
+# 
+# 
+# tid2 = tid %>%
+#   pad(group = 'writer', interval = 'day') %>%   # Explicitly fill by 1 min
+#   fill_by_value(n)
+# 
+# 
+# tid3 = tid2 %>% transform(logical_write = ifelse(n > 0, 1, 0))
+# colnames(tid3)[3] = "n_write"
 
 
 
 
-cafeWritten$date[str_length(cafeWritten$date) == 5] = paste0(gsub("-",".",Sys.Date()),".")
-cafeWritten$date = ymd(cafeWritten$date)
-
-start_date = "2021-01-28"
 
 
-cafeWritten %>% as.data.frame()
-tid = cafeWritten %>% group_by(writer,date) %>% summarise(n = n()) %>% 
-  filter(date >= start_date) 
-
-tid = as.data.frame(tid)
-
-
-
-source("./src/06_members.R", encoding = "UTF-8")
-
-
-for(i in 1:length(writers)) {
-  if(tid %>% filter(writer == writers[i], date == start_date) %>% nrow() != 1) {
-    tid = rbind(tid, data.frame(writer = writers[i], 
-                                date = start_date,
-                                n = NA))
+cafe_write_func = function(data, start_date, end_date) {
+  
+  # cafeWritten = readxl::read_excel("D:/workspace/python_workspace/01_cafeCrawler/naverCafeWrite_A.xlsx")
+  
+  data$date[str_length(data$date) == 5] = paste0(gsub("-",".",end_date),".")
+  data$date = ymd(data$date)
+  
+  data$writer = gsub("'","",data$writer)
+  data$writer = gsub("\\[","",data$writer)
+  data$writer = gsub("\\]","",data$writer)
+  
+  
+  tid = data %>% group_by(writer,date) %>% summarise(n = n()) %>% 
+    filter(date >= start_date) 
+  
+  tid = as.data.frame(tid)
+  
+  
+  
+  
+  for(i in 1:length(writers)) {
+    if(tid %>% filter(writer == writers[i], date == start_date) %>% nrow() != 1) {
+      tid = rbind(tid, data.frame(writer = writers[i], 
+                                  date = start_date,
+                                  n = NA))
+      
+    }
+    
+    if(tid %>% filter(writer == writers[i], date == end_date) %>% nrow() != 1) {
+      tid = rbind(tid, data.frame(writer = writers[i], 
+                                  date = end_date,
+                                  n = NA))
+      
+    }
+    
     
   }
+  tid = tid %>% arrange(writer, date)
+  tid[is.na(tid)] = 0
   
-  if(tid %>% filter(writer == writers[i], date == Sys.Date()) %>% nrow() != 1) {
-    tid = rbind(tid, data.frame(writer = writers[i], 
-                                date = Sys.Date(),
-                                n = NA))
-    
-  }
   
+  tid2 = tid %>%
+    pad(group = 'writer', interval = 'day') %>%   # Explicitly fill by 1 min
+    fill_by_value(n)
+  
+  
+  tid3 = tid2 %>% transform(logical_write = ifelse(n > 0, 1, 0))
+  colnames(tid3)[3] = "n_write"
+  
+  return(tid3)
   
 }
-tid = tid %>% arrange(writer, date)
-tid[is.na(tid)] = 0
 
 
-tid2 = tid %>%
-  pad(group = 'writer', interval = 'day') %>%   # Explicitly fill by 1 min
-  fill_by_value(n)
+cafe_viwer_func = function(data, start_date, end_date) {
+  
+  data$date[str_length(data$date) == 5] = paste0(gsub("-",".",end_date),".")
+  data$date = ymd(data$date)
+  
+  data$writer = gsub("'","",data$writer)
+  data$writer = gsub("\\[","",data$writer)
+  data$writer = gsub("\\]","",data$writer)
+  
+  
+  data$viewCount = gsub(",", "", data$viewCount)
+  data$viewCount = as.integer(data$viewCount)
 
+  tid = data %>% group_by(writer) %>% summarise(ViewCount_total = sum(viewCount)) %>%
+    arrange(desc(ViewCount_total))
 
-tid3 = tid2 %>% transform(logical_write = ifelse(n > 0, 1, 0))
-colnames(tid3)[3] = "n_write"
+  tid = as.data.frame(tid)
 
-
+  return(tid)
+  
+}
